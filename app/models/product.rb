0320814be9802452df3489eec80d8720beabc3b3
Rebecca Qu <beks.qu@gmail.com>
2015-07-04 14:05:36 -0400

    displaying validation errors on the product form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+	# Validates presence of description & name fields
+	# Validates price_in_cents is an integer
 	validates :description, :name, presence: true
 	validates :price_in_cents, numericality: {only_integer: true}
 end
